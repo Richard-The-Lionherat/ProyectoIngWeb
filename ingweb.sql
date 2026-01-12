04730fed548e3d8ed16d6cff552bb9fada8ee0b3
Richard-The-Lionherat <145938554+Richard-The-Lionherat@users.noreply.github.com>
2026-01-12 14:09:03 -0600

    Add files via upload

--- ingweb.sql
+++ ingweb.sql
@@ -519,7 +519,7 @@ DELIMITER ;
 
 DROP TABLE IF EXISTS `categorias`;
 CREATE TABLE `categorias` (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `nombre` varchar(100) NOT NULL,
   `imagen` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
